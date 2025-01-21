624e0ebf0520d4fb75321957bbdd6e0d4281d5cd
Justine Philip <justine.philip6@gmail.com>
2025-01-21 18:48:01 -0500

    Time: 0 ms, Memory: 17.6 MB - LeetHub

---
+++ 11719-1812-746-min-cost-climbing-stairs/11719-1812-746-min-cost-climbing-stairs.py
@@ -0,0 +1,16 @@
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         cost.append(0);
+#         for i in range(len(cost)-3,-1,-1):
+#             cost[i] = cost[i]+min(cost[i+1], cost[i+2]);
+        
+#         return min(cost[0],cost[1]);
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp= [0]*(len(cost)+1);
+        # dp[len(cost)] =0;
+        dp[len(cost)-1]=cost[len(cost)-1];
+        for i in range(len(cost)-2,-1,-1):
+            dp[i]= cost[i]+min(dp[i+1],dp[i+2]);
+        # print(dp)
+        return min(dp[0],dp[1]);
