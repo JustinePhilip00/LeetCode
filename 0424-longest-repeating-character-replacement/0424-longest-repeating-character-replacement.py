dea309752bd23f37ab18d766520ab141b67cf80d
Justine Philip <justine.philip6@gmail.com>
2025-02-21 19:58:45 -0500

    Time: 171 ms (37.92%), Space: 18.2 MB (22.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        mymap={};
         l=0;
-        longest_substring = 0;
-        mymap = {};
-        for r in range(len(s)):
-            
+        res = 0;
+        for r in range(0,len(s)):
             mymap[s[r]] = 1+mymap.get(s[r],0);
-            if ((r-l+1) - max(mymap.values()) > k ):
+            while (r-l+1)-max(mymap.values()) > k:
                 mymap[s[l]] = mymap[s[l]]-1;
                 l=l+1;
-        
-        longest_substring = max(longest_substring, r-l+1);
-        return longest_substring;
-        
-            
+            res = max(res,r-l+1);
+        return res;
+
         
