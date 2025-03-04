b0fccb99e184f165c10505f993ed1fb19adfefaa
Justine Philip <justine.philip6@gmail.com>
2025-03-04 16:02:27 -0500

    Time: 0 ms (100%), Space: 17.6 MB (78.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp=[0]*(n+1); 
-        dp[n]=0;
-        dp[n-1]=1;
-        dp[n-2]=2;
-        for i in range(n-3,-1,-1):
-            dp[i]=dp[i+1]+dp[i+2];
+        dp = [0]*(n+1);
+        dp[n] = 0;
+        dp[n-1]= 1;
+        dp[n-2] =2;
+        for i in range(n-3, -1,-1):
+            dp[i] = dp[i+1] + dp[i+2];
         return dp[0];
-        
-        
-        
-        
+
+
+
+
+
         
