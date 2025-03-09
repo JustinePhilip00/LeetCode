2f746cf94a78546701713349e9681bf540387936
Justine Philip <justine.philip6@gmail.com>
2025-03-09 18:56:50 -0400

    Time: 0 ms (100%), Space: 17.9 MB (17.92%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,21 +6,37 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return [];
+        # if not root:
+        #     return [];
         
-        q= deque([root]);
-        mylist=[];
-        while q:
-            level_size = len(q)
-            for i in range(level_size):
-                node = q.popleft();
-                if i==level_size-1:
-                    mylist.append(node.val);
-                if node.left:
-                    q.append(node.left);
-                if node.right:
-                    q.append(node.right);
+        # q= deque([root]);
+        # mylist=[];
+        # while q:
+        #     level_size = len(q)
+        #     for i in range(level_size):
+        #         node = q.popleft();
+        #         if i==level_size-1:
+        #             mylist.append(node.val);
+        #         if node.left:
+        #             q.append(node.left);
+        #         if node.right:
+        #             q.append(node.right);
                 
-        return mylist;
+        # return mylist;
+        output = [];
+        q = deque();
+        q.append(root);
+
+        while q:
+            rightside = None;
+            qlen = len(q);
+            for i in range(qlen):
+                node = q.popleft();
+                if node:
+                    rightside= node;
+                    q.append(node.left);
+                    q.append(node.right);
+            if rightside:
+                output.append(rightside.val);
+        return output;
         
