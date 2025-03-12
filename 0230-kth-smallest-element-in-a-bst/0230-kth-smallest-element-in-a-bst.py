63761d190a0056aa61fff718b18111b991dc89e1
Justine Philip <justine.philip6@gmail.com>
2025-03-12 16:13:40 -0400

    Time: 2 ms (41.46%), Space: 21.1 MB (72.81%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,33 +4,46 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+# # #         O(n) approach : recursive
+# #         mystack=[];
+# #         def dfs(curr):
+            
+# #             if not curr:
+# #                 return ;
+# #             dfs(curr.left);
+# #             mystack.append(curr.val);
+# #             dfs(curr.right);
+# #             if mystack:
+# #                 for i in range(len(mystack)):
+# #                     if i== k-1:
+# #                         return mystack[i];
+# #         return dfs(root);
+
+# # faster way as it is Iterative DFS method: O(n) space
+#             mystack =[];
+#             curr= root;
+#             while mystack or curr :
+#                 while curr:
+#                     mystack.append(curr);
+#                     curr = curr.left;
+#                 curr= mystack.pop();
+#                 k=k-1;
+#                 if k==0:
+#                     return curr.val;
+#                 curr=curr.right;
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-# #         O(n) approach : recursive
-#         mystack=[];
-#         def dfs(curr):
-            
-#             if not curr:
-#                 return ;
-#             dfs(curr.left);
-#             mystack.append(curr.val);
-#             dfs(curr.right);
-#             if mystack:
-#                 for i in range(len(mystack)):
-#                     if i== k-1:
-#                         return mystack[i];
-#         return dfs(root);
-            mystack =[];
-            curr= root;
-            while mystack or curr :
-                while curr:
-                    mystack.append(curr);
-                    curr = curr.left;
-                curr= mystack.pop();
-                k=k-1;
-                if k==0:
-                    return curr.val;
-                curr=curr.right;
-                
+        mystack = [];
+        def dfs(node):
+            if not node: return ;
+            dfs(node.left)
+            mystack.append(node.val); 
+            if len(mystack) == k:
+                return;
+            dfs(node.right)
+        dfs(root);
+        return mystack[k-1];                
                     
         
