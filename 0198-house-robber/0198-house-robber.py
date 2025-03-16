019687a939c9c90e51a1bf0f81d6ac41c4bc8ebb
Justine Philip <justine.philip6@gmail.com>
2025-03-16 13:51:45 -0400

    Time: 0 ms (100%), Space: 17.9 MB (12.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0]*(len(nums)+1);
-        dp[len(nums)-1] = nums[len(nums)-1];
-        dp[len(nums)-2] = nums[len(nums)-2];
-        for i in range(len(nums)-3,-1,-1):
-            dp[i]= nums[i]+max(dp[i+2],dp[i+3]);
-        # print(dp);    
-        return max(dp[0],dp[1]);
+        # dp = [0]*(len(nums)+1);
+        # dp[len(nums)-1] = nums[len(nums)-1];
+        # dp[len(nums)-2] = nums[len(nums)-2];
+        # for i in range(len(nums)-3,-1,-1):
+        #     dp[i]= nums[i]+max(dp[i+2],dp[i+3]);
+        # # print(dp);    
+        # return max(dp[0],dp[1]);
+
+        rob1 = 0; 
+        rob2= 0;
+        for n in nums:
+            temp = max(n+rob1, rob2);
+            rob1=rob2;
+            rob2 = temp;
+        return rob2;
+
 
 
         
