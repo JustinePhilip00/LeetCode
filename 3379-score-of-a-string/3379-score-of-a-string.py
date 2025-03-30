e44f4112c7cc54251a40518c4abe55c8b18606a9
Justine Philip <justine.philip6@gmail.com>
2025-03-30 12:30:00 -0400

    Time: 3 ms (25.9%), Space: 18 MB (6.29%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ascii = {"a": 97,
+                "b": 98, 
+                "c":99,
+                "d":100,
+                "e":101,
+                "f":102,
+                "g":103,
+                "h":104,
+                "i":105,
+                "j":106,
+                "k":107,
+                "l":108,
+                "m":109,
+                "n":110,
+                "o":111,
+                "p":112,
+                "q":113,
+                "r":114,
+                "s":115,
+                "t":116,
+                "u":117,
+                "v":118,
+                "w":119,
+                "x":120,
+                "y":121,
+                "z":122,
+                }
+        total = 0;
+        for i in range(len(s)):
+            if i+1 < len(s):
+                currscore=abs(ascii[s[i]] - ascii[s[i+1]]);
+                total = total +currscore;
+        return total;
+
+        
