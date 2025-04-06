bedfd6fcc12953d20afd4ab70b4f483ad8c2818e
Justine Philip <justine.philip6@gmail.com>
2025-04-05 20:09:25 -0400

    Time: 3 ms (21.92%), Space: 17.8 MB (51.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,37 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        mymap = { ")":"(", "}":"{","]":"["};
-        mylist = list();
-        for ch in s:
-            if ch not in mymap:
-                mylist.append(ch);
-                continue;
-            if not mylist or mymap[ch]!=mylist[-1]:
-                return False;
-            mylist.pop();
-        return not mylist;
+        # mymap = { ")":"(", "}":"{","]":"["};
+        # mylist = list();
+        # for ch in s:
+        #     if ch not in mymap:
+        #         mylist.append(ch);
+        #         continue;
+        #     if not mylist or mymap[ch]!=mylist[-1]:
+        #         return False;
+        #     mylist.pop();
+        # return not mylist;
+
+        mystack = [];
+        if len(s) ==1:
+            return False;
+        for c in s:
+            if c in "[,(,{":
+                mystack.append(c);
+            if c == "]": 
+                if not mystack or mystack[len(mystack)-1] != "[":
+                    return False;
+                mystack.pop();
+            elif c== ")": 
+                if not mystack or mystack[len(mystack)-1]!= "(":
+                    return False
+                mystack.pop();
+            elif c== "}":
+                if not mystack or mystack[len(mystack)-1]!="{":
+                    return False;
+                mystack.pop();
+            # else:
+            #     return False;
+        return True if len(mystack) == 0 else False;
                 
                 
                 
