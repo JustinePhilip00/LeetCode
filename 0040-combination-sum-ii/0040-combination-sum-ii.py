900c0a30a6a08b47936ea72b21cddcc793c252a3
Justine Philip <justine.philip6@gmail.com>
2025-04-12 13:29:16 -0400

    Time: 31 ms (27.06%), Space: 17.7 MB (84.01%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -4,21 +4,22 @@ class Solution:
         candidates.sort();
         curr=[];
         def dfs(i,curr,total):
-            if total==target:
+            if total == target:
                 res.append(curr.copy());
-                return
-            
-            if total>target or i>=len(candidates):
+                return;
+            if i>=len(candidates) or total > target:
                 return;
             curr.append(candidates[i]);
-            dfs(i+1,curr,total+candidates[i]);
+            dfs(i+1,curr, total+candidates[i]);
             curr.pop();
-            while i+1<len(candidates) and candidates[i]==candidates[i+1]:
+            while i+1< len(candidates) and candidates[i] == candidates[i+1]:
                 i=i+1;
             dfs(i+1,curr,total);
+
         dfs(0,[],0);
         return res;
 
+            
 
 
 
