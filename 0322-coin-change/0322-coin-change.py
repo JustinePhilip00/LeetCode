511041bca17e299bc87c7665c8614b62f4ddcd8e
Justine Philip <justine.philip6@gmail.com>
2025-04-21 23:03:13 -0400

    Time: 855 ms (35.91%), Space: 18.9 MB (31.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -7,12 +7,12 @@ class Solution:
         #         if a-c >= 0:
         #             dp[a] = min(dp[a], 1+dp[a-c]);
         # return dp[amount] if dp[amount]!=amount+1 else -1;
-        dp=[amount+1]*(amount+1);
-        dp[0] =0;
-        # print(dp); 
-        for coin in coins:
-            for i in range(coin,amount+1):
-                dp[i] = min(dp[i],1+dp[i-coin]);
-        
-        # print(dp);
-        return dp[amount] if dp[amount]!=amount+1 else -1;
+        dp = [float('inf')] * (amount+1);
+        dp[0] = 0;
+        print(dp);
+        for i in range(1,amount+1):
+            for c in coins:
+                if i-c >=0:
+                    dp[i] = min(dp[i], 1+dp[i-c]);
+        print(dp);
+        return dp[amount] if dp[amount] != float('inf') else -1;
