30ac5144b94ce4a024c94c9a1c3ea710419635e4
Justine Philip <justine.philip6@gmail.com>
2025-05-31 23:12:24 -0400

    Time: 319 ms (52.52%), Space: 17.6 MB (98.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -13,7 +13,6 @@ class Solution:
                 l =l-1;
                 r=r+1;
         
-        for i in range(len(s)):
             l=i;
             r=i+1;
             while l>=0 and r<len(s) and s[l]==s[r]:
