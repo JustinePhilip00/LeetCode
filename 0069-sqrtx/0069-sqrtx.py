38608fa4373e5283cc55eb2edb1e9eabbdbd4107
Justine Philip <justine.philip6@gmail.com>
2025-06-27 11:53:02 -0400

    Time: 2 ms (65.56%), Space: 17.8 MB (31.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,13 +1,27 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         # diff_so_far = float("inf");
-        num = float("inf")
-        for i in range(0, x+1):
-            if i*i <= x:
-                num = i;
+        # num = float("inf")
+        # for i in range(0, x+1):
+        #     if i*i <= x:
+        #         num = i;
+        #     else:
+        #         break;
+        # return num
+        if x<2:
+            return x;
+        left = 0;
+        right = x//2;
+        while left<=right:
+            mid = (left+right)//2;
+            if mid*mid == x:
+                return mid;
+            elif mid*mid < x:
+                left = mid +1;
             else:
-                break;
-        return num
+                right = mid -1;
+        return right;
+
 
 
 
