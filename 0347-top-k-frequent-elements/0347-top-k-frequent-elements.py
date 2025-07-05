89838b7a5e7d669af896110ffd957312dcfd0bb4
Justine Philip <justine.philip6@gmail.com>
2025-07-05 01:51:26 -0400

    Time: 9 ms (30.24%), Space: 24 MB (25.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -16,6 +16,18 @@ class Solution:
         #         if len(res) == k:
         #             print(res);
         #             return res;
+        count ={};
+        freq = [[] for i in range(len(nums)+1)];
+        for num in nums:
+            count[num] = 1+count.get(num,0);
+        for num,c in count.items():
+            freq[c].append(num);
+        res =[];
+        for i in range(len(freq)-1,0,-1):
+            for num in freq[i]:
+                res.append(num);
+                if len(res) == k:
+                    return res;
         # count = {};
         # for num in nums:
         #     count[num] = 1+count.get(num, 0);
@@ -30,16 +42,16 @@ class Solution:
         # for i in range(k):
         #     res.append(heapq.heappop(heap)[1])
         # return res;
-        mymap ={};
-        res =[];
-        for num in nums:
-            mymap[num] = 1+mymap.get(num, 0);
-        while len(res) < k:
-            top = max(mymap, key = mymap.get);
-            # print(top)
-            res.append(top);
-            mymap.pop(top);
-        return res;
+        # mymap ={};
+        # res =[];
+        # for num in nums:
+        #     mymap[num] = 1+mymap.get(num, 0);
+        # while len(res) < k:
+        #     top = max(mymap, key = mymap.get);
+        #     # print(top)
+        #     res.append(top);
+        #     mymap.pop(top);
+        # return res;
 
 
             
