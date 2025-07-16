030561501cf32b1638c1f7240eedab09d29c6868
Justine Philip <justine.philip6@gmail.com>
2025-07-16 15:46:39 -0400

    Time: 0 ms (100%), Space: 17.9 MB (8.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,7 +5,7 @@ class Solution:
         dp[n-1]= 1;
         dp[n-2] =2;
         for i in range(n-3, -1,-1):
-            dp[i] = dp[i+1] + dp[i+2];
+            dp[i] = dp[i+1]+dp[i+2];
         return dp[0];
 
 
