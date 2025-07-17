f2d0ccdf546a161fdd7abc762083509577d1b1cc
Justine Philip <justine.philip6@gmail.com>
2025-07-16 21:14:52 -0400

    Time: 7 ms (23.92%), Space: 18.6 MB (59.44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,42 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        result = [];
-        ptr1=0;
-        ptr2=len(numbers)-1;
-#         if numbers[ptr1]+numbers[ptr2] > target:
-#             ptr2=ptr2-1;
-#         elif numbers[ptr1]+numbers[ptr2] < target:
-#             ptr1=ptr1+1;
-#         else:
-#             result.append(ptr1+1);
-#             result.append(ptr2+1);
-        while ptr1 < ptr2:
-            if numbers[ptr1]+numbers[ptr2]==target:
-                result.append(ptr1+1);
-                result.append(ptr2+1);
-            
-            if numbers[ptr1]+numbers[ptr2] > target:
-                ptr2=ptr2-1;
-            else:
-                ptr1=ptr1+1;
+        # this is O(n^2) but this causes error if there is occurrence of a number twice
+        # for i,num in enumerate(numbers):
+        #     if (target - num) in numbers:
+        #         return [i+1, numbers.index(target-num)+1];
+
+        ptr1 = 0;
+        ptr2 = len(numbers)-1;
+        while ptr1<ptr2:
+            if numbers[ptr1]+numbers[ptr2] == target:
+                return [ptr1+1, ptr2+1];
+            elif numbers[ptr1]+numbers[ptr2] > target:
+                ptr2 = ptr2-1;
+            elif numbers[ptr1]+numbers[ptr2] < target:
+                ptr1 = ptr1+1;
         
-        return result;
+#         result = [];
+#         ptr1=0;
+#         ptr2=len(numbers)-1;
+# #         if numbers[ptr1]+numbers[ptr2] > target:
+# #             ptr2=ptr2-1;
+# #         elif numbers[ptr1]+numbers[ptr2] < target:
+# #             ptr1=ptr1+1;
+# #         else:
+# #             result.append(ptr1+1);
+# #             result.append(ptr2+1);
+#         while ptr1 < ptr2:
+#             if numbers[ptr1]+numbers[ptr2]==target:
+#                 result.append(ptr1+1);
+#                 result.append(ptr2+1);
+            
+#             if numbers[ptr1]+numbers[ptr2] > target:
+#                 ptr2=ptr2-1;
+#             else:
+#                 ptr1=ptr1+1;
+        
+#         return result;
+
             
             
             
