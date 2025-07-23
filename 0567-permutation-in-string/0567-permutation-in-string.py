6e04d47175888591be5522b52a90f192a123a96b
Justine Philip <justine.philip6@gmail.com>
2025-07-23 18:52:57 -0400

    Time: 19 ms (66.33%), Space: 18.1 MB (19.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) >len(s2):
+        if len(s1) > len(s2):
             return False;
-        s1count = [0] * 26;
-        s2count = [0] *26;
+        s1Count = [0]*26;
+        s2Count = [0]*26;
         for i in range(len(s1)):
-            s1count[ord(s1[i])-ord('a')] = s1count[ord(s1[i])-ord('a')] +1;
-            s2count[ord(s2[i])-ord('a')] = s2count[ord(s2[i])-ord('a')] +1;
-        matches = 0;
+            s1Count[ord(s1[i])-ord('a')] =  s1Count[ord(s1[i])-ord('a')]+1;
+            s2Count[ord(s2[i])-ord('a')] =  s2Count[ord(s2[i])-ord('a')]+1;
+        matches =0;
         for i in range(26):
-            if s1count[i] == s2count[i]:
-                matches = matches + 1 
-        
-        l= 0;
-        for r in range(len(s1),len(s2)):
-            if matches == 26: return True;
-            index = ord(s2[r])-ord('a');
-            s2count[index] = s2count[index]+1;
-            if s1count[index] == s2count[index]:
+            matches = matches+(1 if s1Count[i]==s2Count[i] else 0);
+        l =0;
+        for r in range(len(s1), len(s2)):
+            if matches ==26: return True
+            
+            index = ord(s2[r]) - ord('a');
+            s2Count[index] = s2Count[index] +1;
+            if s1Count[index] == s2Count[index]:
                 matches = matches +1;
-            elif s1count[index] + 1 == s2count[index]:
+            elif s1Count[index] +1 == s2Count[index]:
                 matches = matches -1;
             
-            index = ord(s2[l])-ord('a');
-            s2count[index] = s2count[index]-1;
-            if s1count[index] == s2count[index]:
+            index = ord(s2[l]) - ord('a');
+            s2Count[index] = s2Count[index] -1;
+            if s1Count[index] == s2Count[index]:
                 matches = matches +1;
-            elif s1count[index] - 1 == s2count[index]:
+            elif s1Count[index] -1 == s2Count[index]:
                 matches = matches -1;
             l=l+1;
         return matches ==26;
