aad196fcba5b075d5dff56226cdadf8f119fdc1c
Justine Philip <justine.philip6@gmail.com>
2025-08-21 00:47:15 -0400

    Time: 7 ms (81.25%), Space: 18.9 MB (30.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # newS =""
+        # for ch in s:
+        #     if ch.isalnum():
+        #         newS = newS+ch.lower();
+        # l =0;
+        # r= len(newS)-1;
+        # while l<=r:
+        #     if newS[l]!=newS[r]:
+        #         return False;
+        #     l=l+1;
+        #     r=r-1;
+        # return True
+        # print(s)
         newS =""
-        for ch in s:
-            if ch.isalnum():
-                newS = newS+ch.lower();
+        # print(newS)
+        for word in s.split():
+            for ch in word:
+                if ch.isalnum():
+                    newS += ch.lower()
+        # print(newS)
         l =0;
         r= len(newS)-1;
         while l<=r:
@@ -12,6 +28,9 @@ class Solution:
             l=l+1;
             r=r-1;
         return True
+
+        
+
             
         
             
