2cc68e34b35b77946e6ac2d0e63d5a9919e4df7f
Justine Philip <justine.philip6@gmail.com>
2025-10-18 17:47:48 -0400

    Time: 87 ms (58.36%), Space: 26.9 MB (77.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,7 +3,7 @@ class Solution:
         maxProf = 0;
         curProf = 0;
         buy = prices[0];
-        for r in range(len(prices)):
+        for r in range(1, len(prices)):
             curProf = prices[r]- buy;
             maxProf = max(maxProf, curProf);
             if prices[r]<buy:
