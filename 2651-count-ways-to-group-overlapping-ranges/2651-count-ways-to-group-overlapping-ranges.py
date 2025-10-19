4bb8e46842b431c2a8032eddbd17e1fa07af41df
Justine Philip <justine.philip6@gmail.com>
2025-10-18 22:04:42 -0400

    Time: 19 ms (87.33%), Space: 46.9 MB (79.64%) - LeetHub

--- 2651-count-ways-to-group-overlapping-ranges/2651-count-ways-to-group-overlapping-ranges.py
+++ 2651-count-ways-to-group-overlapping-ranges/2651-count-ways-to-group-overlapping-ranges.py
@@ -4,20 +4,21 @@ class Solution:
         output= [];
         startpt = ranges[0][0];
         endpt=ranges[0][1];
+        count = 0;
         if len(ranges)==1:
             return 2;
-        if len(ranges)>=2:
-            for i in range(1,len(ranges)):
-               start = ranges[i][0];
-               if start<=endpt:
+        for i in range(1,len(ranges)):
+            start = ranges[i][0];
+            if start<=endpt:
                 endpt=max(endpt,ranges[i][1]);
-               else:
-                output.append([startpt,endpt]);
+            else:
+                # output.append([startpt,endpt]);
+                count= count+1;
                 startpt = ranges[i][0];
                 endpt=ranges[i][1];
-            output.append([startpt,endpt]);
-        print(output);
-        return pow(2,len(output),10**9 + 7);
+            # output.append([startpt,endpt]);
+        count=count+1
+        return pow(2,count,10**9 + 7);
 
 
 
